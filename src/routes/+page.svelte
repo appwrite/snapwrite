79df4eb89852c4e9cdd2dbfbe6354f1f5b026866
tglide <26071571+TGlide@users.noreply.github.com>
2023-10-20 12:23:23 +0100

    basic frame

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,4 +1,21 @@
-<h1>Welcome to SvelteKit</h1>
-<p class="bg-red-12">
-	Visit <a href="https://kit.svelte.dev">kit.svelte.dev</a> to read the documentation
-</p>
+<script>
+	import CodeMirror from '$lib/components/CodeMirror.svelte';
+</script>
+
+<div class="grid place-items-center">
+	<div class="frame p-4 bg-gray-8 overflow-hidden w-240 min-h-60">
+		<CodeMirror />
+	</div>
+</div>
+
+<style lang="scss">
+	.frame {
+		@include border-gradient;
+		--p-border-radius: 0.5rem;
+		--p-border-gradient-before: linear-gradient(
+			to bottom,
+			var(--color-red-8) 0%,
+			var(--color-red-3) 100%
+		);
+	}
+</style>
--- vite.config.ts
+++ vite.config.ts
@@ -5,9 +5,16 @@ import UnoCSS from '@unocss/svelte-scoped/vite';
 export default defineConfig({
 	plugins: [
 		UnoCSS({
-			// injectReset: '@unocss/reset/normalize.css', // see type definition for all included reset options or how to pass in your own
+			injectReset: `@unocss/reset/normalize.css` // see type definition for all included reset options or how to pass in your own
 			// ...other Svelte Scoped options
 		}),
 		sveltekit()
-	]
+	],
+	css: {
+		preprocessorOptions: {
+			scss: {
+				additionalData: `@use '$styles' as *;`
+			}
+		}
+	}
 });
