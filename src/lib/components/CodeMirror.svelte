0e0c4d8c7c82e561de7be731b950bad89341f1d0
tglide <26071571+TGlide@users.noreply.github.com>
2023-11-17 14:30:03 +0000

    update colors

--- src/lib/components/CodeMirror.svelte
+++ src/lib/components/CodeMirror.svelte
@@ -73,15 +73,15 @@
 		),
 		syntaxHighlighting(
 			HighlightStyle.define([
-				{ tag: tags.moduleKeyword, color: 'var(--color-accent)' },
+				{ tag: tags.moduleKeyword, color: '#9685FE' },
 				{
 					tag: [tags.function(tags.variableName), tags.function(tags.propertyName)],
 					color: '#67A3FE'
 				},
-				{ tag: [tags.keyword, tags.tagName], color: '#FE9567' },
+				{ tag: [tags.keyword, tags.tagName], color: '#fe86a8' },
 				{ tag: tags.escape, color: '#67A3FE' },
 				{ tag: tags.comment, color: '#6E6E71' },
-				{ tag: tags.propertyName, color: 'var(--color-mint-500)' },
+				{ tag: tags.propertyName, color: '#67A3FE' },
 				{ tag: tags.string, color: '#4AD4AB' }
 			])
 		),
